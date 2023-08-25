d9c6f64ad612228b784261c4362a0e644aa8408c
Falme Streamless <falme_streamless@hotmail.com>
2023-08-25 02:59:30 -0300

    Items purchased appears in player's inventory

---
+++ Assets/Scripts/UI/Store/Inventory/Inventory.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] private GameObject itemObject;
+	[SerializeField] private Transform itemListContent;
+
+	[SerializeField] private ShopItemListData shopItemList;
+
+    void Start()
+    {
+		LoadItems();
+    }
+
+	private void OnEnable() {
+		ShopItem.OnFinishPurchase += RefreshShop;
+	}
+
+	private void OnDisable() {
+		ShopItem.OnFinishPurchase -= RefreshShop;
+	}
+
+	private void Update() {
+		if(Input.GetKeyDown(KeyCode.Alpha4)) RefreshShop();
+	}
+
+	private void LoadItems()
+	{
+		foreach(ShopItemData itemData in shopItemList.shopItemDatas)
+		{
+			if(CheckItemInStore(itemData.ID, itemData.itemCategory)) continue;
+
+			ShopItem item = Instantiate(itemObject, itemListContent).GetComponent<ShopItem>();
+			item.SetItem(itemData);
+		}
+	}
+
+	private void RefreshShop()
+	{
+		foreach(Transform child in itemListContent)
+			Destroy(child.gameObject);
+
+		LoadItems();
+	}
+
+	private bool CheckItemInStore(int ID, ItemCategory itemCategory)
+	{
+		return PlayerPrefs.GetInt($"{(int)itemCategory}_{ID}_owned", 0) == 0;
+	}
+}
